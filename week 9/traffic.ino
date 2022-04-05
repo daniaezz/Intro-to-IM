c53c0f1b4ef2d502adb2486a122e7c7421490ede
dan <90758857+daniaezz@users.noreply.github.com>
2022-04-05 11:54:41 +0400

    Add files via upload

---
+++ week 9/traffic.ino
@@ -0,0 +1,67 @@
+const int REDP = 2;
+const int YELLOWP = 6;
+const int GREENP = 4;
+const int REDB = 8;
+const int YELLOWB = 10;
+const int GREENB = 12;
+const int LIGHT = A0;
+const int BUTTON = 3;
+
+int analogVal = 0;
+int inc = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(REDP, OUTPUT);
+  pinMode(YELLOWP, OUTPUT);
+  pinMode(GREENP, OUTPUT);
+  pinMode(REDB, OUTPUT);
+  pinMode(YELLOWB, OUTPUT);
+  pinMode(GREENB, OUTPUT);
+  pinMode(BUTTON, INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int buttonVal = digitalRead(BUTTON);
+  analogVal = analogRead(LIGHT);
+
+  if (analogVal > 500 and buttonVal == LOW) {
+    digitalWrite(GREENB, HIGH);
+    digitalWrite(REDB, LOW);
+    digitalWrite(YELLOWB, LOW);
+  } else if (buttonVal == LOW and analogVal <= 500) {
+    digitalWrite(GREENB, HIGH);
+    digitalWrite(REDB, LOW);
+    digitalWrite(YELLOWB, HIGH);
+    delay(500);
+    digitalWrite(YELLOWB, LOW);
+    delay(200);
+  } else if (analogVal < 500 and buttonVal == HIGH) {
+
+    digitalWrite(REDB, LOW);
+    for ( int i = 0; i < 6; i++) {
+      inc += 10;
+      analogWrite(YELLOWB, inc);
+      digitalWrite(GREENB, LOW);
+      delay(200);
+      digitalWrite(GREENB, HIGH);
+      delay(200);
+    }
+    delay(500);
+    digitalWrite(GREENB, LOW);
+    digitalWrite(YELLOWB, LOW);
+    digitalWrite(REDB, HIGH);
+    delay(2000);
+    digitalWrite(REDB, LOW);
+    digitalWrite(GREENB, HIGH);
+    delay(2000);
+
+  } else {
+    digitalWrite(GREENB, LOW);
+    digitalWrite(REDB, HIGH);
+    digitalWrite(YELLOWB, LOW);
+  }
+
+
+}

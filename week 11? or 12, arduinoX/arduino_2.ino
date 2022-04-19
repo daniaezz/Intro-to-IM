9d8c8fabf4c884a52ac9f6b1ef7f607c42627d67
dan <90758857+daniaezz@users.noreply.github.com>
2022-04-19 23:17:13 +0400

    Add files via upload

---
+++ week 11? or 12, arduinoX/arduino_2.ino
@@ -0,0 +1,26 @@
+const int LED = 3;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(LED, OUTPUT);
+  while (Serial.available() <= 0) {
+    Serial.println("hello"); // send a starting message
+    delay(300);              // wait 1/3 second
+  }
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+
+    // read information from p5
+    int intByte = Serial.read();
+    if(intByte == 0) {
+      digitalWrite(LED, LOW);
+    } else {
+      digitalWrite(LED, HIGH);
+    }
+
+//    Serial.println(intByte);
+  
+  }
+}

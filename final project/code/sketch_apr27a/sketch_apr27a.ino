a6e9722568e1ac57a52a2c91875712b3a65e1f7e
dan <90758857+daniaezz@users.noreply.github.com>
2022-05-06 01:17:26 +0400

    Update sketch_apr27a.ino

--- final project/code/sketch_apr27a/sketch_apr27a.ino
+++ final project/code/sketch_apr27a/sketch_apr27a.ino
@@ -1,9 +1,11 @@
-void setup() {
-  // put your setup code here, to run once:
+#define FORCE_SENSOR_PIN A0 // the FSR and 10K pulldown are connected to A0
 
+void setup() {
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  int analogReading = analogRead(FORCE_SENSOR_PIN);
+  Serial.println(analogReading); // print the raw analog reading
+  delay(1000);
 }
